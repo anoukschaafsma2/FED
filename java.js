1758390615811ebe64c9eecbf8ec129cf681f811
anoukschaafsma2 <145329936+anoukschaafsma2@users.noreply.github.com>
2025-12-02 22:48:36 +0100

    updated website + readme

--- java.js
+++ java.js
@@ -1,13 +1,5 @@
 let deButton = document.querySelector("menu li:nth-of-type(1) button");
-
 let menu = document.querySelector("header nav:nth-of-type(1)");
-
-
-const ul = document.querySelector('main > ul:nth-of-type(1)');
-const btnLeft = ul.querySelector('li:first-child > button');
-const btnRight = ul.querySelector('li:last-child > button');
-
-const scrollAmount = 400; // pixels per klik
 
 deButton.addEventListener("click", toggleButton);
 
@@ -17,11 +9,5 @@ function toggleButton() {
   document.documentElement.classList.toggle("no-scroll");
 }
 
-btnLeft.addEventListener('click', () => {
-  ul.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-});
 
-btnRight.addEventListener('click', () => {
-  ul.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-});
 
